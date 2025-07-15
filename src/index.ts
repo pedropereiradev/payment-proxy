08b08e6ce2d2926c167d89556818a7f7318c2144
Pedro Pereira <devpereirapedro@gmail.com>
2025-07-14 22:36:17 -0300

    feat: payment api endpoints and health check route

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,49 @@
-console.log("Hello via Bun!");
+import { getPaymentsSummary, processPayment } from "./process-payment";
+
+Bun.serve({
+  routes: {
+    "/payments": {
+      POST: async (req) => {
+        const payment = (await req.json()) as {
+          correlationId: string;
+          amount: number;
+        };
+
+        if (!payment.correlationId || !payment.amount) {
+          return new Response("Missing correlationId or amount", {
+            status: 400,
+          });
+        }
+
+        await processPayment(payment);
+
+        return new Response(null, { status: 201 });
+      },
+    },
+    "/payments-summary": {
+      GET: async (req: Bun.BunRequest) => {
+        const url = new URL(req.url);
+        const params = url.searchParams;
+
+        const from = params.get("from");
+        const to = params.get("to");
+
+        const summary = await getPaymentsSummary(from, to);
+
+        return Response.json(summary);
+      },
+    },
+    "/health": {
+      GET: () => Response.json({ ping: "pong" }),
+    },
+  },
+  error(error) {
+    console.error(error);
+    return new Response(`Internal Error: ${error.message}`, {
+      status: 500,
+      headers: {
+        "Content-Type": "text/plain",
+      },
+    });
+  },
+});
